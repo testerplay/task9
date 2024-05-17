b0b365c2edce8c9b56bfe41f8a007fc1e886f53e
kulag <kulag@DESKTOP-CH7J89A>
2024-05-18 01:00:08 +0300

    Добавьте файлы проекта.

---
+++ task9/task9.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+string doubleCharacter(const char C, const string& S) {
+    string result;
+    for (char ch : S) {
+        result += ch;
+        if (ch == C) {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char C;
+    string S;
+
+    cout << "Введите символ C: ";
+    cin >> C;
+
+    cout << "Введите строку S: ";
+    cin.ignore();  // Игнорируем символ новой строки после ввода символа
+    getline(cin, S);
+
+    string doubledString = doubleCharacter(C, S);
+
+    cout << "Результирующая строка: " << doubledString << endl;
+
+    return 0;
+}
